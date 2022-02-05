102b363145d3e81123935ba279601406707e6995
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-05 20:57:11 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -4,9 +4,9 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Paste Ngrock Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region ap 3389 &>/dev/null &
+nohup ./ngrok tcp --region jp 3389 &>/dev/null &
 echo Please wait downloading file install
-echo "Chờ khoảng 5 phút, nếu thấy lâu quá bấm Enter cái"
+echo "Chờ khoảng 5 phút, nếu thấy lâu quá chưa hiện IP bấm Enter cái"
 sudo apt-get update > /dev/null 2>&1
 echo "Install App"
 sudo apt install firefox -y > /dev/null 2>&1
@@ -19,6 +19,7 @@ sudo apt-get install xarchiver > /dev/null 2>&1
 sudo apt install mkvtoolnix mkvtoolnix-gui > /dev/null 2>&1
 dpkg --add-architecture i386 && apt-get update && apt-get install wine32 > /dev/null 2>&1
 apt-get install wine32 > /dev/null 2>&1
+sudo apt-get install terminator > /dev/null 2>&1
 echo "Install RDP"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
