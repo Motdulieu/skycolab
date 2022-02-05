4dadd996cffd0d118035ff45929df7cf0a8503e3
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-05 20:06:12 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -8,8 +8,17 @@ nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 echo Please wait downloading file install
 echo "Wait 2 Minutes"
 sudo apt-get update > /dev/null 2>&1
-echo "Install Firefox"
+echo "Install App"
 sudo apt install firefox -y > /dev/null 2>&1
+sudo apt-get install axel -y > /dev/null 2>&1
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo dpkg -i google-chrome-stable_current_* > /dev/null 2>&1
+sudo apt-get -f install > /dev/null 2>&1
+sudo apt-get install xarchiver > /dev/null 2>&1
+sudo apt install mkvtoolnix mkvtoolnix-gui > /dev/null 2>&1
+dpkg --add-architecture i386 && apt-get update && apt-get install wine32 > /dev/null 2>&1
+apt-get install wine32 > /dev/null 2>&1
 echo "Install RDP"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
