a6cd12ade5de9c8bf5036936f4d4793a65f85866
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-08 17:55:55 +0700

    Update vps12h.sh

--- vps12h.sh
+++ vps12h.sh
@@ -1,7 +1,7 @@
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste Token Ngrok and Enter: " CRP
+read -p "Paste Token Ngrok and Enter: "
 echo "======================="
 echo choose ngrok region
 echo "======================="
