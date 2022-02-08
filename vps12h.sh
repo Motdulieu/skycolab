1e02b773a9c15565ad0d75418c1062ae98321f07
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-08 17:30:18 +0700

    Update vps12h.sh

--- vps12h.sh
+++ vps12h.sh
@@ -1,7 +1,6 @@
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 echo "======================="
 echo choose ngrok region
 echo "======================="
@@ -39,6 +38,7 @@ sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
 echo "Start RDP"
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
+clear
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
