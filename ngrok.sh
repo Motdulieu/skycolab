657f32f2c52304465bbe352accf9d3d74338f466
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-08 20:23:25 +0700

    Delete ngrok.sh

--- ngrok.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Token Ngrok and Enter: " CRP
-./ngrok authtoken $CRP 
