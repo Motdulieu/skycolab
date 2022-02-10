f72e665849753f68bf0c9ee88575e32f5b577254
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-02-10 12:05:15 +0700

    Update vps12h.sh

--- vps12h.sh
+++ vps12h.sh
@@ -1,6 +1,7 @@
 echo "Download ngrok..."
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+print("Copy&paste your tunnel authtoken from https://dashboard.ngrok.com/auth")
 read -p "Paste Ngrock Authtoken and Enter: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region jp 3389 &>/dev/null &
